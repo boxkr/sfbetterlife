8a01b68821a7250024c7eb58347e2466fbb69efd
MatthewG3 <matthew.gorena@gmail.com>
2022-01-29 18:05:15 -0600

    landing page changes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,41 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import './LandingPage.css';
+import logo from '../images/statefarm-logo-white.png';
+import Button from '@mui/material/Button';
+import {useNavigate} from 'react-router-dom';
+
 
 function LandingPage() {
-
+    const navigate = useNavigate();
     return (
 
-        <div>Landing Page</div>
+        <div>
+
+            <div className='welcome'>
+                <Button 
+                variant="contained"
+                sx={{color: 'black', 
+                    backgroundColor: 'white', 
+                    '&:hover': {
+                        backgroundColor: 'white'}
+                }}
+                onClick={()=> (navigate("/userLogin"))}>
+                    I am a User
+                </Button>
+                <Button variant="contained" 
+                sx={{color: 'black', backgroundColor: 'white', '&:hover': {backgroundColor: 'white'}}}
+                onClick={()=> (navigate("/doctorLogin"))}>
+                    I am a Doctor
+                </Button>
+            </div>
+            
+            
+        </div>
     )
 
 
 }
 
 
-export default LandingPage
+export default LandingPage;
